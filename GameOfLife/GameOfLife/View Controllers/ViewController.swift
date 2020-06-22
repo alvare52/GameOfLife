6311cfad9e824a596c2c4de6c27208b55d6aea42
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-06-22 10:32:09 -0700

    adds project, folders, nav controller

---
+++ GameOfLife/GameOfLife/View Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GameOfLife
+//
+//  Created by Jorge Alvarez on 6/22/20.
+//  Copyright © 2020 Jorge Alvarez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
